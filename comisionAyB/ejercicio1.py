d486370748460db8008e961e00ecae1903c352e1
Jnrivarola <jererivarola.dev@gmail.com>
2026-09-16 19:19:09 -0300

    "Se agregan archivos py"

--- comisionAyB/ejercicio1.py
+++ comisionAyB/ejercicio1.py
@@ -1,4 +1,43 @@
-num1 = 25
+"""num1 = 25
 num1 = "Hola chicos"
 
 print(num1)
+
+#racionales y lógicos
+
+nombre = "Jorge"
+
+if nombre != "Jorge":
+    print("son diferentes")
+else:
+    print("son iguales")"""
+
+#Orden de procedencia    
+"""resultado = (1+2+3+5+4**5)/5 
+print(resultado)"""  
+
+#conversión de tipos
+
+"""numero_entero = ("10")
+resultado = numero_entero + 5
+print(resultado)"""
+
+"""frase1= "Que lindo día"
+frase2= " para caminar"
+concatenacion = frase1 + frase2
+print(concatenacion)"""
+
+#entrada y salida por consola
+
+"""nombre = input("Ingrese su nombre: ")
+print("Hola ", nombre)"""
+
+"""num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+resultado = num1+num2
+print("El resultado de la suma es ",resultado)
+print(f"El resultado de la suma de {num1} y {num2} es: {resultado}")"""
+
+
+
+    
---
+++ comisionAyB/ejercicios_de_practica.py
@@ -0,0 +1,16 @@
+"""Pedir un número al usuario y mostrar
+su cuadrado.
+
+numero = int(input("Ingrese un número: "))
+
+cuadrado = numero ** 2
+
+print(f"El cuadrado de {numero} es: {cuadrado}")"""
+
+#Pedir dos números y mostrar la suma.
+"""num1 = float(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+suma = num1+num2
+
+print("El resultado es: ",suma)"""
