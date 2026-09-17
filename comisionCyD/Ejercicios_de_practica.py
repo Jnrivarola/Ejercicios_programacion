4175cd3671b6b86111135d5a19ce0d00d50efde3
Jnrivarola <jererivarola.dev@gmail.com>
2026-09-17 19:02:28 -0300

    "Se agrega archivo py"

---
+++ comisionCyD/Ejercicios_de_practica.py
@@ -0,0 +1,37 @@
+"""nombre = "Juan"
+
+if nombre != "Juan":
+    print("Son diferentes")
+else:
+    print("Son iguales")
+    
+promedio = (2+3+4+5)+(4/3)"""  
+
+#Conversión de tipo   
+"""numero_entero = int("10")
+resultado = numero_entero + 5
+print(resultado)"""
+
+"""frase1 = "Que lindo día "
+frase2 = "Para correr"
+concatenacion = frase1 + frase2
+print(concatenacion)"""
+
+"""decimal = float("3.14")
+resultado = decimal * 5
+print(resultado)"""
+
+"""numero = 42
+cadena = str(numero)
+print("El número es: " + cadena)"""
+    
+"""print(bool(0)) 
+print(bool(2))
+print(bool(""))"""
+    
+#Input y output
+"""num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+suma = num1+num2
+print("La suma de " ,num1, " y ",num2, " es: ",suma)
+print(f"La suma de {num1} y {num2} es: {suma}")"""
