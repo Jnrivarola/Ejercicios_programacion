64ce0d8830329e48d78b59b8cc29ef34d3c7da4d
Jnrivarola <jererivarola.dev@gmail.com>
2026-09-17 19:05:14 -0300

    "Se agrega saludo en el archivo"

--- comisionCyD/Ejercicios_de_practica.py
+++ comisionCyD/Ejercicios_de_practica.py
@@ -35,3 +35,5 @@ num2 = int(input("Ingrese el segundo número: "))
 suma = num1+num2
 print("La suma de " ,num1, " y ",num2, " es: ",suma)
 print(f"La suma de {num1} y {num2} es: {suma}")"""
+
+#print("Hola")
