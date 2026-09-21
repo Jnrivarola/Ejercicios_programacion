a0a1fa5b1e165ffb9db39d503b3aa614a4c405eb
Jnrivarola <jererivarola.dev@gmail.com>
2026-09-21 17:05:54 -0300

    "Se suben modificaciones en nombre de archivos"

--- comisionCyD/Ejercicios_de_practica.py
+++ comisionCyD/Ejercicios_de_practica.py
@@ -1,44 +1,44 @@
-"""nombre = "Juan"
+nombre = "Juan"
 
 if nombre != "Juan":
     print("Son diferentes")
 else:
     print("Son iguales")
     
-promedio = (2+3+4+5)+(4/3)"""  
+promedio = (2+3+4+5)+(4/3) 
 
 #Conversión de tipo   
-"""numero_entero = int("10")
+numero_entero = int("10")
 resultado = numero_entero + 5
-print(resultado)"""
+print(resultado)
 
-"""frase1 = "Que lindo día "
+frase1 = "Que lindo día "
 frase2 = "Para correr"
 concatenacion = frase1 + frase2
-print(concatenacion)"""
+print(concatenacion)
 
-"""decimal = float("3.14")
+decimal = float("3.14")
 resultado = decimal * 5
-print(resultado)"""
+print(resultado)
 
-"""numero = 42
+numero = 42
 cadena = str(numero)
-print("El número es: " + cadena)"""
+print("El número es: " + cadena)
     
-"""print(bool(0)) 
+print(bool(0)) 
 print(bool(2))
-print(bool(""))"""
+print(bool(""))
     
 #Input y output
-"""num1 = int(input("Ingrese el primer número: "))
+num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
 suma = num1+num2
 print("La suma de " ,num1, " y ",num2, " es: ",suma)
-print(f"La suma de {num1} y {num2} es: {suma}")"""
+print(f"La suma de {num1} y {num2} es: {suma}")
 
 """Pedir un nombre y mostrar cuántas
 letras tiene."""
 
-"""nombre = input("Ingrese su nombre: ")
+nombre = input("Ingrese su nombre: ")
 cantidad_letras = len(nombre)
-print(f"Su nombre tiene {cantidad_letras} letras.")"""
+print(f"Su nombre tiene {cantidad_letras} letras.")
--- comisionCyD/ejemplo_comentarios.py
+++ comisionCyD/ejemplo_comentarios.py
@@ -1,8 +1,11 @@
+#Esto es un comentario de una sola línea
+
 '''Esto es un comentario
 que acabo de crear
 hace poco
  y espero les guste'''
  
+#Frase con 2 tipos de comillas 
 frase = 'Que hermoso día y no "noche"'
 print(frase)
     
