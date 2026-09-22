9164045183f5df2e573b050d11366decf32c7a9c
Jnrivarola <jererivarola.dev@gmail.com>
2026-09-22 19:12:24 -0300

    "Se agregan ejercicios"

---
+++ comisionCyD/conceptos_condicionales.py
@@ -0,0 +1,36 @@
+#Condicional Simple
+"""edad = input("Ingrese su edad: ")
+
+if edad >= 18: 
+    print("Usted es mayor")"""
+    
+#Condicional Compuesta  
+"""edad = int(input("Ingrese su edad: "))  
+
+if edad >= 18: 
+    print("Usted es mayor")
+else:
+    print("Usted es menor")"""
+    
+#Condicional múltiple
+"""edad = int(input("Ingrese su edad: "))
+
+if edad >= 30 and edad <= 90:
+    print("Usted es mayor de edad y además cuenta con un super descuento") 
+elif edad >= 18 and edad <= 29:
+    print("Usted es mayor")
+elif edad >= 1 and edad <= 17:
+    print("Usted es menor")
+else:
+    print("Edad inválida. Debe ser del 1 al 90")"""
+
+"""edad = 17
+
+if (edad >= 18 and edad < 65) or (edad >=65 and edad < 70):
+#      (False  and    True)   or      (False  and   True)   
+#             False           or            False
+#                           False
+    print("Eres elegible para votar.")
+else:
+    print("No podes votar.")"""
+    
